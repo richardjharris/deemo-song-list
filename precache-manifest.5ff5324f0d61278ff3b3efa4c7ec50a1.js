a5e91c1f35fc67282a3530c1ee161d49d77879be
Richard Harris <rjh@netcraft.com>
2020-03-22 11:30:40 +0900

    Initial app build

---
+++ precache-manifest.5ff5324f0d61278ff3b3efa4c7ec50a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd41ba239fad43b89c285c354235b33",
+    "url": "/deemo-song-list/index.html"
+  },
+  {
+    "revision": "c78ba893b4ac087be261",
+    "url": "/deemo-song-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb5a934116132069a170",
+    "url": "/deemo-song-list/static/js/2.2811dc8c.chunk.js"
+  },
+  {
+    "revision": "4771746a683086e85cff1305577cd356",
+    "url": "/deemo-song-list/static/js/2.2811dc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78ba893b4ac087be261",
+    "url": "/deemo-song-list/static/js/main.2efff94e.chunk.js"
+  },
+  {
+    "revision": "9c2c537fafce780bc23d",
+    "url": "/deemo-song-list/static/js/runtime-main.842773b1.js"
+  }
+]);
